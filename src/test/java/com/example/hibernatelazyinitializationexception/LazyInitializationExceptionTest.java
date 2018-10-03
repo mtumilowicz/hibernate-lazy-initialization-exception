4983e2b5252886a9e9166b638fbd1a0be8a24db6
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-03 20:06:52 +0200

    more tests

--- src/test/java/com/example/hibernatelazyinitializationexception/LazyInitializationExceptionTest.java
+++ src/test/java/com/example/hibernatelazyinitializationexception/LazyInitializationExceptionTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
+
 /**
  * Created by mtumilowicz on 2018-10-03.
  */
@@ -19,17 +21,23 @@ public class LazyInitializationExceptionTest {
     
     @Autowired
     EmployeeRepository repository;
+
+    @Test
+    public void noSession_onlyGetter() {
+        repository.save(Employee.builder().id(1).build()); // save in hibernate doesn't need session
+        repository.findById(1).map(Employee::getIssues);
+    }
     
     @Test(expected = LazyInitializationException.class)
-    public void noSession() {
-        repository.save(Employee.builder().id(1).build()); // save in hibernate doesn't need session
-        System.out.println(repository.findById(1));
+    public void noSession_consumingCollection() {
+        repository.save(Employee.builder().id(1).build());
+        repository.findById(1).map(Employee::getIssues).map(Collection::size);
     }
 
     @Test
     @Transactional
     public void transactional() {
-        repository.save(Employee.builder().id(1).build()); // save in hibernate doesn't need session
+        repository.save(Employee.builder().id(1).build());
         System.out.println(repository.findById(1));
     }
     
